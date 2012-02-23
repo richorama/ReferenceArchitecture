6a8799c28744c8f1450e97e6a258b7e4568b0c5d
Richard Astbury <Richard.Astbury@gmail.com>
2012-02-23 16:51:25 +0000

    initial version

---
+++ Two10.TaskList.Web/Controllers/StyleController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Two10.TaskList.Web.Controllers
+{
+    public class StyleController : Controller
+    {
+        public ActionResult Index()
+        {
+            this.Response.ContentType = @"text/css";
+            return Content("");
+        }
+    }
+}
---
+++ Two10.TaskList.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Two10.TaskList.Web.MvcApplication" Language="C#" %>
